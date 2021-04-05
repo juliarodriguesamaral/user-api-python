b6748c60f90dde99fc73458bf3584c938b6e538f
Julia <rodriguesjulia.amaral@gmail.com>
2021-04-04 21:31:22 -0300

    configuração e conexão com o banco de dados

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost:3306/post_python'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:pipoca123@localhost:3306/post_python'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
